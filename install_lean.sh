a5656ce97d1cc83eeb3c275e6e5af3f5b8300c58
Your Name <you@example.com>
2026-04-26 11:27:36 +0000

    push the previous code to archive as the pipeline is wrong

--- install_lean.sh
+++
@@ -1,8 +0,0 @@
-# shell script to install lean
-sudo apt install git curl
-curl https://elan.lean-lang.org/elan-init.sh -sSf | sh -s -- -y
-source $HOME/.elan/env
-lean --version
-lake --version
-git config --global user.email "venkats3@illinois.edu"
-git config --global user.name "venkat"
